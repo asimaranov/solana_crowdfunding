79541e6995f9a39c371b6c2bcf48b539a0168d2b
asimaranov <a.simaranov@gmail.com>
2022-06-06 19:38:28 +0300

    Implemented withdrawing and PDA storaging

--- programs/crypton_solana_crowdfunding/src/lib.rs
+++ programs/crypton_solana_crowdfunding/src/lib.rs
@@ -8,9 +8,62 @@ pub struct CrowdfundingAccount {
     pub balance: u64,
 }
 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer=owner, space=8+32+8)]
+    pub crowdfunding_account: Account<'info, CrowdfundingAccount>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Register<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(init, payer=user, space = 8+8+1, seeds=[b"user-info", user.key().as_ref()], bump)]
+    pub user_info: Account<'info, UserInfo>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct MakeDonation<'info> {
+    #[account(mut)]
+    pub crowdfunding_account: Account<'info, CrowdfundingAccount>,
+    #[account(mut)]
+    pub donater: Signer<'info>,
+    #[account(mut, seeds=[b"user-info", donater.key().as_ref()], bump)]
+    pub donater_info: Account<'info, UserInfo>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UserInfo {
+    pub total_donations: u64,
+    pub bump: u8
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub crowdfunding_account: Account<'info, CrowdfundingAccount>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum CrowdfundingError {
+    #[msg("You're not the owner")]
+    NotTheOwner,
+    #[msg("Nothing to withdraw")]
+    NothingToWithdraw,
+}
+
 #[program]
 pub mod crypton_solana_crowdfunding {
-    use anchor_lang::solana_program::{program::invoke, system_instruction};
+    use anchor_lang::solana_program::{program::invoke, program_error, system_instruction};
 
     use super::*;
 
@@ -19,8 +72,16 @@ pub mod crypton_solana_crowdfunding {
         crowdfunding_account.owner = *ctx.accounts.owner.key;
         Ok(())
     }
+    
+    pub fn register(ctx: Context<Register>) -> Result<()> {
+        //let user_info_account = &mut ctx.accounts.user_info;
+        //user_info_account.total_donations = 0;
+        //user_info_account.bump = *ctx.bumps.get("user-info").unwrap();
+        Ok(())
+    }
 
     pub fn make_donation(ctx: Context<MakeDonation>, amount: u64) -> Result<()> {
+        let user_info = &mut ctx.accounts.donater_info;
         let transfer_instruction = system_instruction::transfer(
             ctx.accounts.donater.key,
             &ctx.accounts.crowdfunding_account.key(),
@@ -37,25 +98,37 @@ pub mod crypton_solana_crowdfunding {
 
         let crowdfunding_account = &mut ctx.accounts.crowdfunding_account;
         crowdfunding_account.balance += amount;
+        user_info.total_donations += amount;
+
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer=owner, space=8+32+64)]
-    pub crowdfunding_account: Account<'info, CrowdfundingAccount>,
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        if ctx.accounts.crowdfunding_account.owner != ctx.accounts.owner.key() {
+            return Err(error!(CrowdfundingError::NotTheOwner));
+        };
 
-#[derive(Accounts)]
-pub struct MakeDonation<'info> {
-    #[account(mut)]
-    pub crowdfunding_account: Account<'info, CrowdfundingAccount>,
-    #[account(mut)]
-    pub donater: Signer<'info>,
-    pub system_program: Program<'info, System>,
+        if ctx.accounts.crowdfunding_account.balance == 0 {
+            return Err(error!(CrowdfundingError::NothingToWithdraw));
+        }
 
+        let transfer_instruction = system_instruction::transfer(
+            &ctx.accounts.crowdfunding_account.key(),
+            ctx.accounts.owner.key,
+            ctx.accounts.crowdfunding_account.balance,
+        );
+
+        invoke(
+            &transfer_instruction,
+            &[
+                ctx.accounts.crowdfunding_account.to_account_info(),
+                ctx.accounts.owner.to_account_info(),
+            ],
+        )?;
+
+        let crowdfunding_account = &mut ctx.accounts.crowdfunding_account;
+        crowdfunding_account.balance = 0;
+
+        Ok(())
+    }
 }
--- tests/crypton_solana_crowdfunding.ts
+++ tests/crypton_solana_crowdfunding.ts
@@ -3,13 +3,13 @@ import { Program } from "@project-serum/anchor";
 import { BN } from "bn.js";
 import { expect } from "chai";
 import { CryptonSolanaCrowdfunding } from "../target/types/crypton_solana_crowdfunding";
+import { PublicKey } from '@solana/web3.js';
 
 describe("Crowdfunding test", () => {
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider);
 
   const program = anchor.workspace.CryptonSolanaCrowdfunding as Program<CryptonSolanaCrowdfunding>;
-
   let crowdfundingKeypair: anchor.web3.Keypair;
   const owner = (program.provider as anchor.AnchorProvider).wallet;
   let donater: anchor.web3.Keypair;
@@ -22,13 +22,25 @@ describe("Crowdfunding test", () => {
   });
 
   it("Test service initialization", async () => {
-
     await program.methods.initialize().accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
       .signers([crowdfundingKeypair]).rpc();
 
     let crowdfundingState = await program.account.crowdfundingAccount.fetch(crowdfundingKeypair.publicKey);
     expect(crowdfundingState.balance.eq(new BN(0)));
     expect(crowdfundingState.owner.equals(owner.publicKey)).to.be.true;
+  });
+
+  it("Test registration", async () => {
+    await provider.connection.confirmTransaction(await provider.connection.requestAirdrop(donater.publicKey, testSolAmount * anchor.web3.LAMPORTS_PER_SOL));
+    const initialDonaterBalance = await provider.connection.getBalance(donater.publicKey);
+    expect(initialDonaterBalance).to.equal(testSolAmount * anchor.web3.LAMPORTS_PER_SOL);
+
+    await program.methods.initialize()
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
+      .signers([crowdfundingKeypair]).rpc();
+
+    const [userInfoPda, _] = await PublicKey.findProgramAddress([anchor.utils.bytes.utf8.encode("user-info"), donater.publicKey.toBuffer()], program.programId);
+    await program.methods.register().accounts({user: donater.publicKey, userInfo: userInfoPda}).signers([donater]).rpc();
   });
 
   it("Test donation", async () => {
@@ -40,9 +52,12 @@ describe("Crowdfunding test", () => {
       .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
       .signers([crowdfundingKeypair]).rpc();
 
+    const [userInfoPda, _] = await PublicKey.findProgramAddress([anchor.utils.bytes.utf8.encode("user-info"), donater.publicKey.toBuffer()], program.programId);
+    await program.methods.register().accounts({user: donater.publicKey, userInfo: userInfoPda}).signers([donater]).rpc();
+  
     await program.methods
       .makeDonation(new BN(testSolAmount))
-      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, donater: donater.publicKey })
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, donater: donater.publicKey, donaterInfo: userInfoPda })
       .signers([donater])
       .rpc();
 
@@ -51,4 +66,30 @@ describe("Crowdfunding test", () => {
     expect(crowdfundingState.balance.eq(new BN(testSolAmount))).to.be.true;
     expect(initialDonaterBalance - testSolAmount * anchor.web3.LAMPORTS_PER_SOL).to.be.lte(await provider.connection.getBalance(donater.publicKey));
   });
+
+  it("Test donation in case of insufficient funds", async () => {
+    await program.methods.initialize()
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
+      .signers([crowdfundingKeypair]).rpc();
+
+    await program.methods
+      .makeDonation(new BN(testSolAmount))
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, donater: donater.publicKey })
+      .signers([donater])
+      .rpc().catch(err => expect(err).to.exist);
+
+  });
+
+  it("Check that owner can't withdraw money if no one donated", async () => {
+    await program.methods.initialize()
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
+      .signers([crowdfundingKeypair]).rpc();
+
+    await program.methods.withdraw()
+      .accounts({ crowdfundingAccount: crowdfundingKeypair.publicKey, owner: owner.publicKey })
+      .rpc().catch(err => expect(err.error.errorMessage).to.equal("Nothing to withdraw"));
+  });
+
+  
+
 });
